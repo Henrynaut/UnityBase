b6599b2df30c9d78f0468b9777a1f3a0771ca2e9
Neil McHenry <neilmchenry@gmail.com>
2018-10-03 17:48:46 -0500

    Rigid Body Physics for Buildings, JSON
    Added in rigid body physics simulation for the buildings, as well as started with JSON saver and loader scripts

---
+++ Assets/Scripts/DataHandling/saveJSON.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class saveJSON : MonoBehaviour {
+
+	// Use this for initialization
+	//When Enabled, this script saves all building object data to a JSON
+	//save file that can be loaded into the scene as a sort of city layout
+
+	public Vector3 location;
+	public Vector3 rotation;
+	public int buildingID;
+
+	void Start () {
+		//Find all game objects with "(Clone)" in the Scene
+		//Save Location, Rotation, & BuildingID of each object
+		location = new Vector3(1.0f,2.0f,3.0f); 
+	}
+	
+}
