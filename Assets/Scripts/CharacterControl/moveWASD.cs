7168c006ec886762a3c8624db3485f4157857d13
Neil McHenry <neilmchenry@gmail.com>
2018-10-10 20:23:02 -0500

    WASD Movement
    Added in WASD Movement

---
+++ Assets/Scripts/CharacterControl/moveWASD.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveWASD: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(
+			Input.GetAxis("Horizontal")*Time.deltaTime,
+			Input.GetAxis("UpDown")*Time.deltaTime, 
+			Input.GetAxis("Vertical")*Time.deltaTime
+		);
+	}
+}
