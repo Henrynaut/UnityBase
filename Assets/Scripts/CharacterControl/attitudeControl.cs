db7a23c1f3b1de7a5da91f4955f9685a15d94dad
Henrynaut <neilmchenry@gmail.com>
2018-10-22 15:23:43 -0500

    attitudeControl.cs Created
    For rigid body rotation control in local space

---
+++ Assets/Scripts/CharacterControl/attitudeControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attitudeControl : MonoBehaviour {
+
+public float rotationSpeed;
+public float roll = 0.0f;
+public float pitch = 0.0f;
+public float yaw = 0.0f;
+
+public bool activateRotation = true;
+
+	// Use this for initialization
+	void Start () {
+		rotationSpeed = 0.1f;
+		roll = 8.8f;
+		pitch = 0.0f;
+		yaw = 8.2f;
+	}
+	
+	// Update is called once per frame
+	protected void Update () {
+		if(activateRotation == true)
+		{
+		// roll
+		transform.Rotate(0f, 0f, roll, Space.Self);
+	
+		// pitch
+		transform.Rotate(pitch, 0f, 0f, Space.Self);
+	
+		// yaw
+		transform.Rotate(0f, yaw, 0f, Space.Self);
+		}
+		//Stop Rotation if bool is False
+		if(activateRotation == false){
+		roll = 0f;
+		pitch = 0f;
+		yaw = 0f;
+		}  
+	}
+}
