1bfd13d09e8b845c9db0ef149056e55583830f9a
Neil McHenry <neilmchenry@gmail.com>
2018-10-31 19:05:14 -0500

    Fixed saveJSON Bug

--- Assets/Scripts/DataHandling/saveJSON.cs
+++ Assets/Scripts/DataHandling/saveJSON.cs
@@ -8,20 +8,20 @@ public class saveJSON : MonoBehaviour {
 	//When Enabled, this script saves all building object data to a JSON
 	//save file that can be loaded into the scene as a sort of city layout
 
-	[Serializable]
-	public class SceneData
-	{
-		public Vector3 location;
-		public Vector3 rotation;
-		public int buildingID;
-	}
+	// [Serializable]
+	// public class SceneData
+	// {
+	// 	public Vector3 location;
+	// 	public Vector3 rotation;
+	// 	public int buildingID;
+	// }
 
 	void Start () {
 		//Find all game objects with "(Clone)" in the Scene
 		//Save Location, Rotation, & BuildingID of each object
 		//Convert the data into a json format
-		location = new Vector3(1.0f,2.0f,3.0f);
-		string json = JsonUtility.ToJson(SceneData); 
+		// location = new Vector3(1.0f,2.0f,3.0f);
+		// string json = JsonUtility.ToJson(SceneData); 
 		//Need to convert this into an Unreal-type format as well
 	}
 	
