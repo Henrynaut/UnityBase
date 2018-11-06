b25460b22b8e657a13386c500443b98aff294fc8
Henrynaut <neilmchenry@gmail.com>
2018-11-05 18:18:11 -0600

    Added Teleoperation Monitors

--- Assets/Scripts/CharacterControl/attitudeControl.cs
+++ Assets/Scripts/CharacterControl/attitudeControl.cs
@@ -5,38 +5,38 @@ using UnityEngine;
 public class attitudeControl : MonoBehaviour {
 
 public float rotationSpeed;
-public float roll = 0.0f;
-public float pitch = 0.0f;
-public float yaw = 0.0f;
+public float rollVelocity = 0.0f;
+public float pitchVelocity = 0.0f;
+public float yawVelocity = 0.0f;
 
 public bool activateRotation = true;
 
 	// Use this for initialization
 	void Start () {
 		rotationSpeed = 0.1f;
-		roll = 0.0f;
-		pitch = 0.0f;
-		yaw = 0.0f;
+		rollVelocity = 0.0f;
+		pitchVelocity = 0.0f;
+		yawVelocity = 0.0f;
 	}
 	
 	// Update is called once per frame
 	protected void Update () {
 		if(activateRotation == true)
 		{
-		// roll
-		transform.Rotate(0f, 0f, roll, Space.Self);
-		// pitch
-		transform.Rotate(pitch, 0f, 0f, Space.Self);
+		// rollVelocity
+		transform.Rotate(0f, 0f, rollVelocity, Space.Self);
+		// pitchVelocity
+		transform.Rotate(pitchVelocity, 0f, 0f, Space.Self);
 	
-		// yaw
-		yaw = yaw + rotationSpeed*Input.GetAxis("Yaw")*Time.deltaTime;		
-		transform.Rotate(0f, yaw, 0f, Space.Self);
+		// yawVelocity
+		// yawVelocity = yawVelocity + rotationSpeed*Input.GetAxis("yawVelocity")*Time.deltaTime;		
+		transform.Rotate(0f, yawVelocity, 0f, Space.Self);
 		}
 		//Stop Rotation if bool is False
 		if(activateRotation == false){
-		roll = 0f;
-		pitch = 0f;
-		yaw = 0f;
+		rollVelocity = 0f;
+		pitchVelocity = 0f;
+		yawVelocity = 0f;
 		}  
 	}
 }
