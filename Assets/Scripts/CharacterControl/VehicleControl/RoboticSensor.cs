ed775d9432650dcaa1cd3ff9304395ef300f2c80
Neil McHenry <neilmchenry@gmail.com>
2018-12-19 15:02:49 -0600

    Added RoboticSensor.cs
    For Outputting Rotation and Position of anything the RoboticSensor.cs script is attached to

---
+++ Assets/Scripts/CharacterControl/VehicleControl/RoboticSensor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoboticSensor : MonoBehaviour {
+
+[SerializeField]
+public Vector3 position;
+[SerializeField]
+public Vector3 rotation;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		position = this.transform.position;
+		rotation = (this.transform.rotation).eulerAngles;
+	}
+}
