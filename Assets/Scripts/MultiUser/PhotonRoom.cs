ec19fde1fe697e658f3a9e1e5b2470d252e8303a
Neil McHenry <neilmchenry@gmail.com>
2019-03-21 14:15:05 -0500

    Configured Photon Room RPC Messages

--- Assets/Scripts/MultiUser/PhotonRoom.cs
+++ Assets/Scripts/MultiUser/PhotonRoom.cs
@@ -2,6 +2,7 @@
 using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManager;
 
@@ -19,7 +20,7 @@ public class PhotonRoom : MonoBehaviourPunCallbacks, IInRoomCallbacks
     public int usersInRoom;
     public int myNumberInRoom;
 
-    public int userInSim;
+    public int usersInSim;
 
     //Delayed Start
     private bool readyToCount;
@@ -48,6 +49,7 @@ public class PhotonRoom : MonoBehaviourPunCallbacks, IInRoomCallbacks
     public override void OnEnable(){
         base.OnEnable();
         PhotonNetwork.AddCallbackTarget(this);
+        //Call the Event Listener
         SceneManager.sceneLoaded += OnSceneFinishedLoading;
     }
 
@@ -69,7 +71,29 @@ public class PhotonRoom : MonoBehaviourPunCallbacks, IInRoomCallbacks
 
     // Update is called once per frame
     void Update () {
+        if(MultiUserSettings.multiUserSettings.delayStart){
+            if(usersInRoom == 1){
+                RestartTimer();
+            }
+            if(!isSimLoaded){
+                //Check to see if we are ready to start counting down
+                if(readyToStart){
+                    atMaxUsers -= Time.deltaTime;   //Decrement Time
+                    lessThanMaxUsers = atMaxUsers;
+                    timeToStart = atMaxUsers;
+                }
+                else if (readyToCount){
+                    lessThanMaxUsers -= Time.deltaTime;
+                    timeToStart = lessThanMaxUsers;
+                }
+                Debug.Log("Display time to start to the users" + timeToStart);
+                if(timeToStart <= 0){
+                    //Once timer hits 0, start the simulation
+                    StartSim();
+                }
 
+            }
+        }
     }
 
 	public override void OnJoinedRoom(){
@@ -92,13 +116,86 @@ public class PhotonRoom : MonoBehaviourPunCallbacks, IInRoomCallbacks
                 if (!PhotonNetwork.IsMasterClient){
                     return;
                 }
-                //Closesthe room so that no new players can join the room until it is re-opened
+                //Closes the room so that no new users can join the room until it is re-opened
                 PhotonNetwork.CurrentRoom.IsOpen = false;
             }
         }
         else
         {
-            StartGame();
+            StartSim();
         }
 	}
+
+    public override void OnUserEnteredRoom(Player newUser){
+        base.OnUserEnteredRoom(newUser);
+        Debug.Log("A new user has joined the room");
+        photonUsers = PhotonNetwork.UserList;
+        usersInRoom++;
+        if(MultiUserSettings.multiUserSettings.delayStart){
+            Debug.Log("Displaying users in room out of max users possible (" + usersInRoom + ":" + MultiUserSettings.multiUserSettings.maxUsers + ")");
+            if(usersInRoom > 1)
+            {
+                readyToCount = true;
+            }
+            if(usersInRoom == MultiUserSettings.multiUserSettings.maxUsers){
+                readyToStart = true;
+                //If not the Master Client, return
+                if (!PhotonNetwork.IsMasterClient){
+                    return;
+                }
+                //Closes the room so that no new users can join the room until it is re-opened
+                PhotonNetwork.CurrentRoom.IsOpen = false;
+            }
+        }
+    }
+
+    //Start Simulation Function
+    void StartSim()
+    {
+        isSimLoaded = true;
+        if (!PhotonNetwork.IsMasterClient){
+            return;
+            }
+        if(MultiUserSettings.multiUserSettings.delayStart){
+            //Closes the room so that no new users can join the room until it is re-opened
+            PhotonNetwork.CurrentRoom.IsOpen = false;
+        }
+        PhotonNetwork.LoadLevel(MultiUserSettings.multiUserSettings.multiUserScene)
+    }
+
+    void RestartTimer(){
+        lessThanMaxUsers = startingTime;
+        timeToStart = startingTime;
+        atMaxUsers = 6;
+        readyToCount = false;
+        readyToStart = false;
+    }
+
+    void OnSceneFinishedLoading(Scene scene, LoadSceneMode mode){
+        currentScene = scene.buildIndex;
+        if(currentScene == MultiUserSettings.multiUserSettings.multiUserScene){
+            isSimLoaded = true;
+
+            if(MultiUserSettings.multiUserSettings.delayStart){
+                //Sends an RPC call (message) to the Master Client to create all user game objects
+                PV.RPC("RPC_LoadedSimScene", RpcTarget.MasterClient);
+            }
+            else{
+                RPC_CreateUser();
+            }
+        }
+    }
+
+    [PunRPC]
+    private void RPC_LoadedSimScene(){
+        usersInSim++;
+        if(usersInSim == PhotonNetwork.UserList.Length){
+            PV.RPC("RPC_CreateUser", RpcTarget.All);
+        }
+    }
+
+    [PunRPC]
+    private void RPC_CreateUser(){
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PhotonNetworkPlayer", transform.position, quaternion.identity, 0));
+    }
 }
