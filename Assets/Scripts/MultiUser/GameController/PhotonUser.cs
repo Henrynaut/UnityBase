c77984a1cf56f7825ffc6ff11e1abe2443bf3ae3
Neil McHenry <neilmchenry@gmail.com>
2019-03-21 15:33:59 -0500

    Setup User Avatars and Spawn Points

---
+++ Assets/Scripts/MultiUser/GameController/PhotonUser.cs
@@ -0,0 +1,28 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class PhotonUser : MonoBehaviour
+{
+    private PhotonView PV;
+    public GameObject myAvatar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        int spawnPicker = Random.Range(0, SimSetup.SS.spawnPoints.Length);
+        if(PV.IsMine){
+            myAvatar = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "UserAvatar"),
+                 SimSetup.SS.spawnPoints[spawnPicker].position, SimSetup.SS.spawnPoints[spawnPicker].rotation, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MultiUser/GameController/SimSetup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimSetup : MonoBehaviour
+{
+    //Singleton
+    public static SimSetup SS;
+
+    public Transform[] spawnPoints;
+
+    private void OnEnable(){
+        if(SimSetup.SS == null){
+            SimSetup.SS = this;
+        }
+    }
+}
