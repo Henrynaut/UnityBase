99d6c2d41706c1d8ccb1eaca4d6c433bee4377c3
Neil McHenry <neilmchenry@gmail.com>
2019-03-23 23:01:38 -0500

    Added Username Field

--- Assets/Scripts/MultiUser/AvatarControl/AvatarSetup.cs
+++ Assets/Scripts/MultiUser/AvatarControl/AvatarSetup.cs
@@ -2,11 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class AvatarSetup : MonoBehaviour {
     private PhotonView PV;
     public int characterValue;
+    public string usernameString;
     public GameObject myCharacter;
+    public TextMeshProUGUI myUsername;
 
     // Start is called before the first frame update
     void Start() {
@@ -18,10 +21,15 @@ public class AvatarSetup : MonoBehaviour {
         }
     }
 
+    public void OnUsernameChanged(string usernameIn){
+		usernameString = usernameIn;
+	}
+
     [PunRPC]
     void RPC_AddCharacter(int whichCharacter){
         //Save the Character Selection ID and instantiate
         characterValue = whichCharacter;
         myCharacter = Instantiate(UserInfo.UI.allCharacters[whichCharacter], transform.position, transform.rotation, transform);
+        // myUsername.text = usernameString;
     }
 }
