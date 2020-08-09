987d811df8c33ac0c3013902cbfb83f9c76a21a8
Neil McHenry <neilmchenry@gmail.com>
2020-08-08 22:59:21 -0500

    Instantiate Spline Points Locally
    RPC call sends initial position of each spline sphere to each client, rather than continuously replicating the positio nand velocity of each spline sphere

--- Assets/Scripts/MultiUser/AvatarControl/AvatarInteraction.cs
+++ Assets/Scripts/MultiUser/AvatarControl/AvatarInteraction.cs
@@ -13,7 +13,8 @@ public class AvatarInteraction : MonoBehaviour
     private AvatarSetup avatarSetup;
     public Transform rayOrigin;
     public int lastSplineID;
-    public GameObject SplineSphere;
+    public GameObject splinePrefab;
+    public PhotonMessageInfo Info;
     // public TextMeshProUGUI energyText;
 
     // public TextMeshProUGUI energyDisplay;
@@ -34,8 +35,8 @@ public class AvatarInteraction : MonoBehaviour
 
         //Spawn Transparent Sphere on Left Click
         if(Input.GetMouseButton(0))        {
-            PV.RPC("RPC_Laser_Sphere", RpcTarget.All);
-            // energyText.text = "25 Wh";
+            updateSplinePen();
+            // PV.RPC("RPC_Laser_Sphere", RpcTarget.All);
         }
 
         //Spawn Arrow on Right Click
@@ -50,53 +51,44 @@ public class AvatarInteraction : MonoBehaviour
         }
     }
 
-    // [PunRPC]
-    // void SplineDraw(){
+    [PunRPC]
+    void SplineDraw( Vector3 position, Quaternion rotation, int SplineID, PhotonMessageInfo info )
+    {
+        double timestamp = PhotonNetwork.Time;
+        timestamp = info.SentServerTime;
+        //Spawn Spline sphere
+        Instantiate(splinePrefab, position, rotation);
+        Debug.Log(SplineID);
+    }
 
-    // }
+    void updateSplinePen(){
+        // Instead of having a different PhotonView for each
+        //     sphere, such as in the RPC_Laser_Sphere() function
+        //     I should have all of the spheres be a component
+        //     part of the networked user's PhotonView so
+        //     that I don't run out of PhotonView IDs
+        lastSplineID++;         //Increment lastSplineID by 1
 
-    // void GetSplineSpawnPosition(){
-
-    // }
-
-    // [PunRPC]
-    // void RPC_Laser_Sphere() {
-    //     // Spawn chalk sphere in front of user
-    //     // PhotonNetwork.Instantiate(
-    //         Object.Instantiate(
-    //         Path.Combine("PhotonPrefabs", "Tele_Sphere"),
-    //         (rayOrigin.position + (rayOrigin.TransformDirection(Vector3.forward)*2)),
-    //         //Random Z Rotation
-    //         Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)), 0);
-    //     Debug.Log("Drawing with Chalk");
-    // }
-
-    // void updateSplinePen(){
-    //     // Instead of having a different PhotonView for each
-    //     //     sphere, such as in the RPC_Laser_Sphere() function
-    //     //     I should have all of the spheres be a component
-    //     //     part of the networked user's PhotonView so
-    //     //     that I don't run out of PhotonView IDs
-    //     lastSplineID++;         //Increment lastSplineID by 1
-
-    //     if( PhotonNetwork.OfflineMode == true)
-    //     {
-    //         //SplineDraw() Offline Mode
-    //         SplineDraw( (rayOrigin.position + (rayOrigin.TransformDirection(Vector3.forward)*2))
-    //                   , lastSplineID
-    //                   , null  );
-    //     }
-    //     else
-    //     {
-    //         //SplineDraw() RPC Online Mode
-    //         PV.RPC( "SplineDraw"
-    //                       , RpcTarget.All
-    //                       , new object[] { (rayOrigin.position + (rayOrigin.TransformDirection(Vector3.forward)*2))
-    //                                      , lastSplineID
-    //                                      }
-    //                       );
-    //     }
-    // }
+        if( PhotonNetwork.OfflineMode == true)
+        {
+            //SplineDraw() Offline Mode
+            SplineDraw( (rayOrigin.position + (rayOrigin.TransformDirection(Vector3.forward)*2))
+                      , Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360))
+                      , lastSplineID
+                      , Info );
+        }
+        else
+        {
+            //SplineDraw() RPC Online Mode
+            PV.RPC( "SplineDraw"
+                          , RpcTarget.All
+                          , new object[] { (rayOrigin.position + (rayOrigin.TransformDirection(Vector3.forward)*2))
+                                         , Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360))
+                                         , lastSplineID
+                                         }
+                          );
+        }
+    }
 
     [PunRPC]
     void RPC_Laser_Arrow() {
@@ -137,5 +129,17 @@ public class AvatarInteraction : MonoBehaviour
             Debug.Log("Did Not Hit");
         }
     }
+
+    // [PunRPC]
+    // void RPC_Laser_Sphere() {
+    //     // Spawn chalk sphere in front of user
+    //     // PhotonNetwork.Instantiate(
+    //         Object.Instantiate(
+    //         Path.Combine("PhotonPrefabs", "Tele_Sphere"),
+    //         (rayOrigin.position + (rayOrigin.TransformDirection(Vector3.forward)*2)),
+    //         //Random Z Rotation
+    //         Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)), 0);
+    //     Debug.Log("Drawing with Chalk");
+    // }
 
 }
