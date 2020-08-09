4d28652dcbad09f5a8d00bbc9cc588f8bbdeadd1
Neil McHenry <neilmchenry@gmail.com>
2020-08-09 17:20:05 -0500

    Implemented Mute toggle button "M"

---
+++ Assets/Scripts/MultiUser/VoiceControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using Photon.Voice.Unity;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceControl : MonoBehaviour
+{
+
+    public Recorder VoiceRecorder;
+
+    // Print state of voice recorder
+    void Start()
+    {
+        Debug.Log("Recorder state = " + VoiceRecorder.TransmitEnabled);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            VoiceRecorder.TransmitEnabled = !VoiceRecorder.TransmitEnabled;
+            if (VoiceRecorder.TransmitEnabled)
+            {
+                Debug.Log("Microphone Unmuted.");
+            }
+            else
+            {
+                Debug.Log("Microphone Muted.");
+            }
+        }
+    }
+
+}
