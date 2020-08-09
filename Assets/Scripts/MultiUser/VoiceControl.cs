3692c74c9909741724ed04b43af5e59b5a58bc2e
Neil McHenry <neilmchenry@gmail.com>
2020-08-09 18:21:22 -0500

    Added Muted and Unmuted Icon

--- Assets/Scripts/MultiUser/VoiceControl.cs
+++ Assets/Scripts/MultiUser/VoiceControl.cs
@@ -7,26 +7,63 @@ public class VoiceControl : MonoBehaviour
 {
 
     public Recorder VoiceRecorder;
+    private GameObject mutedIcon;
+    private GameObject unmutedIcon;
 
     // Print state of voice recorder
     void Start()
     {
+        VoiceRecorder.TransmitEnabled = false;       //Muted state by default
         Debug.Log("Recorder state = " + VoiceRecorder.TransmitEnabled);
+        mutedIcon = GameObject.Find("MutedIcon");
+        unmutedIcon = GameObject.Find("UnmutedIcon");
+        muteIconUpdate(VoiceRecorder.TransmitEnabled);
     }
 
     void Update()
     {
+        //Find GameObjects if they were lost on level change
+        if (mutedIcon == null)
+        {
+            mutedIcon = GameObject.Find("MutedIcon");
+            muteIconUpdate(VoiceRecorder.TransmitEnabled);
+            Debug.Log("Found MutedIcon");
+        }
+        if (unmutedIcon == null)
+        {
+        unmutedIcon = GameObject.Find("UnmutedIcon");
+        VoiceRecorder.TransmitEnabled = true;           //Unmute upon level load
+        muteIconUpdate(VoiceRecorder.TransmitEnabled);
+        Debug.Log("Found UnmutedIcon");
+        }
+
         if (Input.GetKeyDown(KeyCode.M))
         {
             VoiceRecorder.TransmitEnabled = !VoiceRecorder.TransmitEnabled;
             if (VoiceRecorder.TransmitEnabled)
             {
                 Debug.Log("Microphone Unmuted.");
+                muteIconUpdate(VoiceRecorder.TransmitEnabled);
             }
             else
             {
                 Debug.Log("Microphone Muted.");
+                muteIconUpdate(VoiceRecorder.TransmitEnabled);
             }
+        }
+    }
+
+    void muteIconUpdate(bool transmitState)
+    {
+        if (transmitState)
+        {
+            unmutedIcon.SetActive(true);        //toggle unmuted icon On
+            mutedIcon.SetActive(false);
+        }
+        else
+        {
+            mutedIcon.SetActive(true);          //toggle muted icon on
+            unmutedIcon.SetActive(false);
         }
     }
 
