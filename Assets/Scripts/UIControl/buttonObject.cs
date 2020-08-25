b43ef9ccfbb62f8a39bc0f21478ec6f02e8826bc
Neil McHenry <neilmchenry@gmail.com>
2020-08-25 01:34:43 -0500

    Created buttonObject script to make mute icon into a button

---
+++ Assets/Scripts/UIControl/buttonObject.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+ 
+ public class buttonObject : MonoBehaviour {
+ 
+    public GameObject definedButton;
+    public GameObject otherButton;
+
+    public Camera Camera;
+
+    public GraphicRaycaster canvasRaycaster;
+    public List<RaycastResult> list;
+    public Vector2 screenPoint;
+ 
+     // Use this for initialization
+     void Start () {    
+         definedButton = this.gameObject;
+     }
+     
+     // Update is called once per frame
+    void Update () {
+
+        //If left mouse is clicked, print name of UI element 
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Graphic Raycast setup
+            list = new List<RaycastResult>();
+            screenPoint = Input.mousePosition;
+            Debug.Log(screenPoint);
+            PointerEventData ed = new PointerEventData(EventSystem.current);
+            ed.position = screenPoint;
+            canvasRaycaster.Raycast(ed, list);
+
+            //If something is hit, print the object name
+            if (list != null && list.Count > 0)
+            {
+                Debug.Log("Hit: " + list[0].gameObject.name);
+
+                //If the hit canvas object is the definedButton, toggle to otherbutton
+                if (list[0].gameObject.name == definedButton.name)
+                {
+                    definedButton.SetActive(false);
+                    otherButton.SetActive(true);
+                    Debug.Log("Toggling " + otherButton.name + " on.");
+                }
+            }
+            else
+            {
+                Debug.Log("No Canvas Object Hit.");
+            }
+        }  
+
+    }
+
+ }
+
+ 
