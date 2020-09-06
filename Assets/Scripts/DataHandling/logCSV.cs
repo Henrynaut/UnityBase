cb0ece0ba018c39ac337fdaedd1e28e871eed389
Neil McHenry <neilmchenry@gmail.com>
2020-09-05 21:52:14 -0500

    Created logCSV class
    For saving data to a local CSV file

---
+++ Assets/Scripts/DataHandling/logCSV.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class logCSV : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        addRecord("124", "Mercy", "56", "cake.txt");
+    }
+
+    public static void addRecord(string ID, string name, string age, string filepath)
+    {
+        try
+        {
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@filepath, true))
+            {
+                file.WriteLine(ID + "," + name + "," + age);
+            }
+        }
+        catch(Exception ex)
+        {
+            throw new ApplicationException("This program did an oopsie :" ex);
+        }
+    }
+}
