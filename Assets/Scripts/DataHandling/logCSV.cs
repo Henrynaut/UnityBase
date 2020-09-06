69aa35c23c080c4836b2da3521cfb01551e8ab57
Neil McHenry <neilmchenry@gmail.com>
2020-09-05 22:35:54 -0500

    Added logCSV to UserAvatar and added ID & Time logging

--- Assets/Scripts/DataHandling/logCSV.cs
+++ Assets/Scripts/DataHandling/logCSV.cs
@@ -9,25 +9,44 @@ using UnityEngine;
 
 public class logCSV : MonoBehaviour
 {
+    private AvatarSetup avatarSetup;
+    public Camera avatarCamera;
+    private string currentTime;
+    private string ID;
+    private int intID;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Get Avatar Camera
+        if (avatarCamera == null)
+        {
+        avatarCamera = GameObject.Find("AvatarCamera").GetComponent<Camera>();
+        }
+
+        //Intialize ID to 0
+        intID = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        addRecord("124", "Mercy", "56", "cake.txt");
+        
+        intID++;                                    //Increment ID by 1
+        ID = intID.ToString();                      //Convert intID to a string
+        currentTime = Time.time.ToString("f6");     //Save game time in seconds as a string
+
+        addRecord(ID, currentTime, "56", "avatarData.csv");
     }
 
-    public static void addRecord(string ID, string name, string age, string filepath)
+    public static void addRecord(string ID, string time, string age, string filepath)
     {
         try
         {
             using (System.IO.StreamWriter file = new System.IO.StreamWriter(@filepath, true))
             {
-                file.WriteLine(ID + "," + name + "," + age);
+                file.WriteLine(ID + "," + time + "," + age);
             }
             Debug.Log("CSV Information saved.");
         }
