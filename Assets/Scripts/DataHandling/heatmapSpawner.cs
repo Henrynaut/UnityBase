b83ff7c6916956e617a16ee03222df23c460fe97
Neil McHenry <neilmchenry@gmail.com>
2020-09-09 19:47:32 -0500

    Spawn whiteboard and nonwhiteboard data

--- Assets/Scripts/DataHandling/heatmapSpawner.cs
+++ Assets/Scripts/DataHandling/heatmapSpawner.cs
@@ -9,12 +9,21 @@ public class heatmapSpawner : MonoBehaviour
     public List<string> LocationsX;
     public List<string> LocationsY;
     public List<string> LocationsZ;
-    public GameObject sphere;
+    public List<string> LocationsXb;
+    public List<string> LocationsYb;
+    public List<string> LocationsZb;
+    public GameObject heatmapSphere;
+    public GameObject heatmapSphereb;
+    
+    public GameObject whiteboard;
+    public GameObject nonWhiteboard;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        using(var reader = new StreamReader(@"D:\Github\UnityBase\heatmapData\1212_locationData.csv"))
+        using(var reader = new StreamReader(@"D:\Github\UnityBase\heatmapData\combined.csv"))
 
         {
             List<string> listA = new List<string>();
@@ -30,16 +39,50 @@ public class heatmapSpawner : MonoBehaviour
                 listB.Add(values[4]);
                 listC.Add(values[5]);
 
-                LocationsZ = listA;
+                LocationsX = listA;
                 LocationsY = listB;
                 LocationsZ = listC;
 
             }
         }
+        using(var reader = new StreamReader(@"D:\Github\UnityBase\heatmapData\combinedb.csv"))
 
-        // for (int i = 0; i = 270; i++) {
-        //     Instantiate(sphere, new Vector3(i, 0, 0), Quaternion.identity);
-        //  }
+        {
+            List<string> listAb = new List<string>();
+            List<string> listBb = new List<string>();
+            List<string> listCb = new List<string>();
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+                listAb.Add(values[3]);
+                listBb.Add(values[4]);
+                listCb.Add(values[5]);
+
+                LocationsXb = listAb;
+                LocationsYb = listBb;
+                LocationsZb = listCb;
+
+            }
+        }
+
+        for (int i = 0; i < 6228; i++) {
+            GameObject newSphere = (GameObject)Instantiate(heatmapSphere, new Vector3(
+                float.Parse(LocationsX[i]),
+                float.Parse(LocationsY[i]),
+                float.Parse(LocationsZ[i])), Quaternion.identity);
+            newSphere.transform.parent = whiteboard.transform;
+         }
+
+        for (int i = 0; i < 9221; i++) {
+            GameObject newSphereb = (GameObject)Instantiate(heatmapSphereb, new Vector3(
+                float.Parse(LocationsXb[i]),
+                float.Parse(LocationsYb[i]),
+                float.Parse(LocationsZb[i])), Quaternion.identity);
+            newSphereb.transform.parent = nonWhiteboard.transform;
+         }
     }
 
     // Update is called once per frame
